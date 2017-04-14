af042a93be6bc8c0e8847aa59a2c0b1d1fb69b73
Stephanie <stephykins2gmail.com>
2017-04-14 10:54:31 -0400

    refactor setQuery

--- public/bundle.js
+++ public/bundle.js
@@ -12246,30 +12246,19 @@ var Nav = function (_Component) {
   }, {
     key: 'setQuery',
     value: function setQuery(key, value) {
-      var props = {};
-
-      if (value === '2010 - Present') {
-        props.year1 = '2010-01-01';
-        props.year2 = new Date().getFullYear() + '-01-01';
-      } else if (key === 'Date' && value) {
-        props.year1 = value.substr(0, 4) + '-01-01';
-        props.year2 = value.substr(7, 4) + '-12-31';
-      } else if (key === 'Date') {
-        props.year1 = value;
-        props.year2 = value;
-      } else if (key === 'Min Rating') {
-        props.rating = value;
-      } else {
-        props[key.toLowerCase()] = value.toLowerCase();
-      }
-
-      this.props.setQuery(props);
+      // set query state, formatted to send to API
+      this.props.setQuery({
+        genre: key === 'Genre' ? value.toLowerCase() : '',
+        rating: key === 'Min Rating' ? value : '',
+        year1: key === 'Date' && value ? value.substr(0, 4) + '-01-01' : '',
+        year2: key === 'Date' && value ? value.substr(7, 4) + '-12-31' : ''
+      });
     }
   }, {
     key: 'render',
     value: function render() {
       var genres = ['Action', 'Adventure', 'Animated', 'Comedy', 'Documentary', 'Drama', 'Fantasy', 'Foreign', 'Horror', 'Romance', 'Sci-fi', 'Thriller'];
-      var dates = ['2010 - Present', '2000 - 2009', '1990 - 1999', '1980 - 1989', '1970 - 1979', '1960 - 1969', '1950 - 1959', '1940 - 1949', '1930 - 1939', '1920 - 1929'];
+      var dates = ['2010 - ' + new Date().getFullYear(), '2000 - 2009', '1990 - 1999', '1980 - 1989', '1970 - 1979', '1960 - 1969', '1950 - 1959', '1940 - 1949', '1930 - 1939', '1920 - 1929'];
       var ratings = ['8.0', '7.0', '6.0', '5.0'];
 
       return _react2.default.createElement(
