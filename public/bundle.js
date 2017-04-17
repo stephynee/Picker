177a44942113947dc0b09c3fb8b3892a3fec7c62
Stephanie <stephykins2gmail.com>
2017-04-17 09:00:51 -0400

    fix bug where saved button did not display

--- public/bundle.js
+++ public/bundle.js
@@ -14076,7 +14076,7 @@ var Nav = function (_Component) {
       var genres = ['Action', 'Adventure', 'Animated', 'Comedy', 'Documentary', 'Drama', 'Fantasy', 'Foreign', 'Horror', 'Romance', 'Sci-fi', 'Thriller'];
       var dates = ['2010 - ' + new Date().getFullYear(), '2000 - 2009', '1990 - 1999', '1980 - 1989', '1970 - 1979', '1960 - 1969', '1950 - 1959', '1940 - 1949', '1930 - 1939', '1920 - 1929'];
       var ratings = ['8.0', '7.0', '6.0', '5.0'];
-      var savedButton = this.props.savedMovies.length > 1 ? _react2.default.createElement(
+      var savedButton = this.props.savedMovies.length > 0 ? _react2.default.createElement(
         _reactRouterDom.Link,
         { to: '/saved', className: 'btn save-btn' },
         'Saved'
